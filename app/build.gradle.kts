b897d1a1977a6bbf339e2180a25b10524a859816
Cedric <cedricctf11a@gmail.com>
2025-07-03 17:26:19 +0800

    added google dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,6 +43,10 @@ dependencies {
     implementation(platform(libs.firebase.bom))
     implementation(libs.firebase.firestore)
     
+    // Google Maps dependency
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.0.1")
+    
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
